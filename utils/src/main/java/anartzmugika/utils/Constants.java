12eb9490c2eeac4a04cc867c0dfc2721d08030a4
Anartz Muxika Ledo <mugan86@gmail.com>
2016-11-24 12:25:03 +0100

    Add new constants

--- utils/src/main/java/anartzmugika/utils/Constants.java
+++ utils/src/main/java/anartzmugika/utils/Constants.java
@@ -1,8 +1,9 @@
 package anartzmugika.utils;
 
-/**
- * Created by anartzmugika on 24/11/16.
- */
+/***************************************************************************************************
+ * Created by Anartz Mugika (mugan86@gmail.com) on 24/11/16.
+ * Constants values to use in library
+ ***************************************************************************************************/
 
 public class Constants {
 
@@ -11,4 +12,13 @@ public class Constants {
     public static final String CONNECT_TO_WIFI = "WIFI";
     public static final String CONNECT_TO_MOBILE = "MOBILE";
     public static final String NOT_CONNECT = "NOT_CONNECT";
+
+    //Data Preferences values
+    //Default constants values
+    public static final String DEFAULT_PREFERENCES = "Default Preferences";
+    public static final String LANG_PROPERTY = "Language";
+    public static final String DEFAULT_LANG = "es";
+
+    //Other data
+    public static final String EMPTY_VALUE = "";
 }
