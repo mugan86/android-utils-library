6079c7dfd0f27fa814a7f5d3e2d11e1879b6bcbb
Anartz Muxika Ledo <mugan86@gmail.com>
2016-11-24 11:31:13 +0100

    Add WebViewActivity to show url content in Android App

---
+++ utils/src/main/java/anartzmugika/utils/activities/WebViewActivity.java
@@ -0,0 +1,98 @@
+package anartzmugika.utils.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import anartzmugika.utils.R;
+
+
+/**
+ * Created by anartzmugika on 8/7/16.
+ */
+
+public class WebViewActivity extends AppCompatActivity {
+    private WebView myWebView;
+    private Toolbar toolbar;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /*****************************************************************
+         *
+         * IMPORTANT!!! To use webview to load urls necesary Internet
+         * connection permission active in Manifest
+         *
+         *  <uses-permission android:name="android.permission.INTERNET"/>
+         *****************************************************************/
+        setContentView(R.layout.webview);
+
+
+        if (Build.VERSION.SDK_INT >= 20) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.mendiak_buttons));
+        }
+
+        myWebView = (WebView) findViewById(R.id.myWebView);
+        if( savedInstanceState == null ) {
+            String url =
+                    getIntent().getStringExtra("url").replace("webview://", "http://");
+            // do something with this URL.
+            loadURL(url);
+
+            //toolbar = ToolbarManage.getTitleSubtitleABToolbar(WebViewActivity.this, (Toolbar) findViewById(R.id.toolbar), "Mendiak", url, true, "");
+        }
+    }
+
+
+
+    private void loadURL(String url)
+    {
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        System.out.println("Load URL: " + url);
+
+        /*
+
+        toolbar.setVisibility(View.VISIBLE);*/
+
+        myWebView.loadUrl(url);
+        myWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView viewx, String urlx) {
+                viewx.loadUrl(urlx);
+                return false;
+            }
+        });
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onCreateWindow(WebView view, boolean dialog, boolean userGesture, android.os.Message resultMsg)
+            {
+                WebView.HitTestResult result = view.getHitTestResult();
+                String data = result.getExtra();
+                Context context = view.getContext();
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+                context.startActivity(browserIntent);
+                return false;
+            }
+        });
+
+
+    }
+
+}
+
+
