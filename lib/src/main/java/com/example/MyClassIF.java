a63a2ae3fb64d4abbff633951fdddc0b40bee523
Anartz Muxika Ledo <mugan86@gmail.com>
2016-11-24 10:25:43 +0100

    Add MyClass Example Interface

---
+++ lib/src/main/java/com/example/MyClassIF.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Created by anartzmugika on 24/11/16.
+ */
+
+public interface MyClassIF {
+    /**
+     * @return only print "hello!!" message
+     */
+    String sayHello();
+
+    /**
+     * @return print hello with parameter (name)
+     */
+    String sayHello(String name);
+}
