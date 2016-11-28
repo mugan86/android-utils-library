de0dfec81cc5dddf8d210b5310e960ae4b1b5efa
Anartz Muxika Ledo <mugan86@gmail.com>
2016-11-28 13:31:44 +0100

    Update

--- themoviedb/src/main/java/com/mugan86/api/TheMovieDBIF.java
+++ themoviedb/src/main/java/com/mugan86/api/TheMovieDBIF.java
@@ -37,7 +37,7 @@ public interface TheMovieDBIF {
      * /discover/movie?sort_by=popularity.desc&vote_count.gte=200
      * @param vote_count: If number positive check greater than else less than. For example: -100 -> vote_count.lte / 100 -> vote_count.gte
      * @param page_limit -1 OR 1 = 1 / 0 = 1500 (UNLIMITED) / total_pages > 1 => specific total_pages
-     * @return Movies List
+     * @return Movies List Text
      */
     public ArrayList<Movie> getMostPopularMovies(int vote_count, int page_limit);
 
