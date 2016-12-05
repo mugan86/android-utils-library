bec9b2f416b09ca785693c5991955333ad2a55cb
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-05 13:01:00 +0100

    Define action after receive location (use extends to override sendDataToCurrentActivity

---
+++ app/src/main/java/anartzmugika/library/CurrentLocationReceiver.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package anartzmugika.library;
+
+import android.content.Context;
+
+import anartzmugika.utils.location.LocationReceiver;
+
+/**
+ * Created by anartzmugika on 5/12/16.
+ */
+
+public class CurrentLocationReceiver extends LocationReceiver {
+
+    @Override
+    public void sendDataToCurrentActivity(double lat, double lng, Context context)
+    {
+        if (context.getClass() == MainActivity.class) ((MainActivity)context).receiveLocation(lat, lng);
+    }
+}
