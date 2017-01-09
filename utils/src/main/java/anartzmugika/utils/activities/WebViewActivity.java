ffe31484f87c6fdcfd7643ce8903e7331086b776
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-09 07:31:11 +0100

    Remove unused code

--- utils/src/main/java/anartzmugika/utils/activities/WebViewActivity.java
+++ utils/src/main/java/anartzmugika/utils/activities/WebViewActivity.java
@@ -2,7 +2,6 @@ package anartzmugika.utils.activities;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -16,20 +15,18 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import anartzmugika.utils.R;
 
 
-/**
+/***************************************************************************************************
  * Created by anartzmugika on 8/7/16.
- */
+ * Activity to execute pass url to show in app internal browser
+ **************************************************************************************************/
 
 public class WebViewActivity extends AppCompatActivity {
     private WebView myWebView;
     private Toolbar toolbar;
-    private LinearLayout loadDataLinearLayout;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,13 +65,11 @@ public class WebViewActivity extends AppCompatActivity {
 
 
 
-    /*private void loadURL(String url)
+    private void loadURL(String url)
     {
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         System.out.println("Load URL: " + url);
-
-        /*
 
         toolbar.setVisibility(View.VISIBLE);
 
@@ -100,71 +95,8 @@ public class WebViewActivity extends AppCompatActivity {
         });
 
 
-    }*/
-
-    private void loadURL(final String url)
-    {
-        System.out.println("Load URL: " + url);
-
-        final boolean[] loadingFinished = {true};
-        final boolean[] redirect = {false};
-
-        //toolbar.setVisibility(View.VISIBLE);
-
-        myWebView.loadUrl(url);
-        myWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView viewx, String urlx) {
-                viewx.loadUrl(urlx);
-                if (!loadingFinished[0]) {
-                    redirect[0] = true;
-                }
-
-                loadingFinished[0] = false;
-                //myWebView.loadUrl(url);
-                return false;
-            }
-
-            @Override
-            public void onPageStarted(
-                    WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                Toast.makeText(getApplicationContext(), "Start load page...", Toast.LENGTH_LONG).show();
-                //SHOW LOADING IF IT ISNT ALREADY VISIBLE
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if(!redirect[0]){
-                    loadingFinished[0] = true;
-                }
-
-                if(loadingFinished[0] && !redirect[0]){
-                    //HIDE LOADING IT HAS FINISHED
-                    Toast.makeText(getApplicationContext(), "Finish load page...", Toast.LENGTH_LONG).show();
-                    //loadDataLinearLayout.setVisibility(View.GONE);
-                    myWebView.setVisibility(View.VISIBLE);
-                } else{
-                    redirect[0] = false;
-                }
-            }
-        });
-
-        myWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onCreateWindow(WebView view, boolean dialog, boolean userGesture, android.os.Message resultMsg)
-            {
-                WebView.HitTestResult result = view.getHitTestResult();
-                String data = result.getExtra();
-                Context context = view.getContext();
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
-                context.startActivity(browserIntent);
-                return false;
-            }
-        });
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
     }
+
 
 }
 
