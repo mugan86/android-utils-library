4fe48395123e5dc6fc6804d51e7e8739d432f72a
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-09 09:21:15 +0100

    Add constants

--- utils/src/main/java/anartzmugika/utils/Constants.java
+++ utils/src/main/java/anartzmugika/utils/Constants.java
@@ -13,6 +13,12 @@ public class Constants {
     public static final String CONNECT_TO_MOBILE = "MOBILE";
     public static final String NOT_CONNECT = "NOT_CONNECT";
 
+
+    //NETWORK TYPE CONNECTIONS
+    public static int TYPE_WIFI = 1;
+    public static int TYPE_MOBILE = 2;
+    public static int TYPE_NOT_CONNECTED = 0;
+
     //Data Preferences values
     //Default constants values
     public static final String DEFAULT_PREFERENCES = "Default Preferences";
@@ -25,4 +31,15 @@ public class Constants {
     //LOCATION
 
     public static final int REQUEST_TO_LOCALIZATION_DEVICE = 101;
+
+    //Urls
+    public static final String GOOGLE_PLAY_MARKET_REFERENCE_WITH_ID = "market://details?id=%s";
+    public static final String GOOGLE_PLAY_MAIN_PAGE_WITH_ID = "http://play.google.com/store/apps/details?id=%s";
+
+    //DATE FORMATS
+    public static final String ONLY_DATA = "yyyy-MM-dd";
+    public static final String DATA_AND_TIME = "yyyy-MM-dd HH:mm:ss";
+    public static final String ONLY_HOUR_WITH_SECONDS = "HH:mm:ss";
+    public static final String ONLY_HOUR_WITHOUT_SECONDS = "HH:mm";
+
 }
