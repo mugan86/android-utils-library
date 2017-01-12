4c45f22583d741065ed76beb597ff7b383f64039
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-12 08:09:24 +0100

    Add new functions to map manage

--- utils/src/main/java/anartzmugika/utils/models/MapModel.java
+++ utils/src/main/java/anartzmugika/utils/models/MapModel.java
@@ -4,7 +4,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 /***************************************************************************************************
  * Created by anartzmugika on 11/1/17.
@@ -71,6 +74,13 @@ public class MapModel{
         getGoogleMap().moveCamera(CameraUpdateFactory.newLatLng(location));
     }
 
+
+    //Center location with max distance center and apply zoom
+    public void moveCamera(LatLng location, float max_distance)
+    {
+        getGoogleMap().moveCamera(CameraUpdateFactory.newLatLngZoom(location, getUseZoomInMap(max_distance)));
+    }
+
     /**********************************************************************************************
      *
      * @param type Define select map type
@@ -85,6 +95,44 @@ public class MapModel{
         if (type == 2) getGoogleMap().setMapType(GoogleMap.MAP_TYPE_HYBRID);
         if (type == 3) getGoogleMap().setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         if (type == 4) getGoogleMap().setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+    }
+
+    public LatLng getMapCenterPosition(int type, ArrayList<PersonModel> person_data)
+    {
+        //To do this, first calculate the bounds of all the markers like so:
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+        for (PersonModel marker : person_data) {
+            builder.include(marker.getPosition());
+        }
+
+        LatLngBounds bounds = builder.build();
+        return bounds.getCenter();
+    }
+
+    public int getUseZoomInMap(float max_distance)
+    {
+        max_distance = max_distance / 1000;
+
+        System.out.println("Max distance in kms: " + max_distance);
+        if (max_distance < 34) {
+            return 10;
+        } else if (max_distance > 24 && max_distance < 48) {
+            return 9;
+        } else if (max_distance > 48 && max_distance < 128) {
+            return 8;
+        } else if (max_distance > 128 && max_distance < 400) {
+            return 7;
+        } else if (max_distance > 400 && max_distance < 600) {
+            return 6;
+        } else if (max_distance > 600 && max_distance < 1300) {
+            return 5;
+        } else if (max_distance > 1300 && max_distance < 2500) {
+            return 4;
+        } else if (max_distance > 2500 && max_distance < 4000) {
+            return 3;
+        }
+        return 2;
     }
 
 
